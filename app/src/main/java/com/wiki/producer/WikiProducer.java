332414e9a946be188318747a0912979a1f1ae9c6
Jack Boyd <jackkellyboyd@gmail.com>
2026-05-19 22:15:39 -0500

    create producer

--- app/src/main/java/com/wiki/producer/WikiProducer.java
+++ app/src/main/java/com/wiki/producer/WikiProducer.java
@@ -1,7 +1,15 @@
 package com.wiki.producer;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 import java.util.Properties;
 
@@ -11,5 +19,27 @@ public class WikiProducer {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://stream.wikimedia.org/v2/stream/recentchange")
+                .build();
+
+        EventSourceListener listener = new EventSourceListener() {
+            @Override
+            public void onEvent(@NonNull EventSource eventSource, @Nullable String id, @Nullable String type, @NonNull String data) {
+            }
+
+            @Override
+            public void onFailure(@NonNull EventSource eventSource, @Nullable Throwable t, @Nullable Response response) {
+            }
+
+            @Override
+            public void onClosed(@NonNull EventSource eventSource) {
+            }
+        };
     }
 }
